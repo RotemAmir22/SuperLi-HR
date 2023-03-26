ca109276143f730a9cc91a6e3be649fa7654b763
AmitAlfasi <127674589+AmitAlfasi@users.noreply.github.com>
2023-03-26 12:45:28 +0300

    1234

---
+++ dev/TransitSystem.java
@@ -0,0 +1,11 @@
+import java.util.List;
+public class TransitSystem {
+    List <ATruck> trucksList;
+    List <Site> sitesList;
+    List <Order> orderList;
+    List <Transit> transitList;
+    List <Driver> driverList;
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
