7a36eafb6f40fca47561bb18261784fa0098daa3
AmitAlfasi <127674589+AmitAlfasi@users.noreply.github.com>
2023-03-26 13:33:42 +0300

    12345

--- dev/Site.java
+++ dev/Site.java
@@ -1,6 +1,6 @@
 public class Site {
     private String address;
-    private Area;
+    private Area areaCode;
     private String contactName;
     private String ContactNumber;
 }
--- dev/TransitSystem.java
+++ dev/TransitSystem.java
@@ -2,9 +2,9 @@ import java.util.List;
 public class TransitSystem {
     List <ATruck> trucksList;
     List <Site> sitesList;
-    List <Order> orderList;
     List <Transit> transitList;
     List <Driver> driverList;
+    List <Order> orderList;
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
