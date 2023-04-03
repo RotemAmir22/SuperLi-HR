eb6dd9065ebd3bf32df16e74573563d5fa961fe9
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-04-03 12:55:54 +0300

    docstring ShiftM_Permissions

--- dev/src/Module_HR/ShiftM_Permissions.java
+++ dev/src/Module_HR/ShiftM_Permissions.java
@@ -1,18 +1,21 @@
 package Module_HR;
 
 /**
- * This class should save the shift managers permissions.
+ * This class creates the shift managers permissions.
  * Every shift manager include 2 objects from this type (one for cancellations, another for manage the team).
  */
 public class ShiftM_Permissions {
 
+    //variables
     private String name;
     private String description;
 
+    //constructor
     public ShiftM_Permissions(String name, String description) {
         this.name = name;
         this.description = description;
     }
+
     //getters
     public String getName() {return name;}
     public String getDescription() {return description;}
