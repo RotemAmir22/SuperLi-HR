134a5789b92547aadb943cc29e4ef4af7d78c3c3
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-04-03 12:57:30 +0300

    docstring Role

--- dev/src/Module_HR/Role.java
+++ dev/src/Module_HR/Role.java
@@ -1,6 +1,9 @@
 package Module_HR;
 
+/**
+ * roles that can be scheduled in every branch store
+ * and qualifications of each employee can be one of the following
+ */
 public enum Role {
-
     GENERAL, CASHIER, STORAGE, DRIVER, SHIFTMANAGER, STOCK, MAID, SECURITY
 }
