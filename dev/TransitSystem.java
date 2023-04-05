30eb8076fc7f0dc064a921888c88a46e4a2fec05
AmitAlfasi <127674589+AmitAlfasi@users.noreply.github.com>
2023-04-05 12:27:54 +0300

    05/04/2023

--- dev/TransitSystem.java
+++ dev/TransitSystem.java
@@ -2,34 +2,37 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-public class TransitSystem {
-    Set<Truck> trucksSet;
-    Set<Supplier> suppliersSet;
-    Set<Store> storeSet;
-    Set<Transit> transitSet;
-    Set<Driver> driverSet;
-    Set<OrderDocument> orderSet;
-    Set<Product> productsSet;
 
-    public void createTruck(){
+/**
+ * UI
+ */
+public class TransitSystem {
+
+
+    public void DetailsTruck(){
         String plateNumber;
         TruckModel model;
         Set<Qualification> qSet;
         double truckWeight;
         double maxWeight;
+
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("-----Create new Truck-----");
         System.out.println("Enter truck plate number: ");
         plateNumber = scanner.nextLine();
+
         System.out.println("Enter truck model: "); //valid model and weight
         String sModel = scanner.nextLine();
+
         System.out.println("Enter truck weight: ");
         truckWeight = scanner.nextDouble();
+
         System.out.println("Enter truck max carry weight: ");
         maxWeight = scanner.nextDouble();
 
         System.out.println("Enter truck qualification: ");
-        //String sModel = scanner.nextLine();
+        String sQuali = scanner.nextLine();
 
     }
     public void createOrderDoc(){}; //switchCase
