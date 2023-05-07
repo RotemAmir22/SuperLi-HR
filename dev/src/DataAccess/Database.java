529559149f7f3ec3d4ed3da7408e37268d9e1d42
Noamshalev1 <113979512+Noamshalev1@users.noreply.github.com>
2023-05-07 13:25:08 +0300

    Update DAO_Employee

--- dev/src/DataAccess/Database.java
+++ dev/src/DataAccess/Database.java
@@ -2,7 +2,7 @@ package DataAccess;
 
 import java.sql.*;
 public class Database {
-    private static final String DB_URL = "jdbc:sqlite:test.db";
+    private static final String DB_URL = "jdbc:sqlite:C:/Users/PC/Documents/GitHub/ADSS_Group_H/identifier.sqlite";
     public static Connection connect() throws SQLException {return DriverManager.getConnection(DB_URL);
     }
 }
