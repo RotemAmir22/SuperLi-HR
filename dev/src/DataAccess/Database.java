aa5a135d1a5f99d742336a80c82136f3636a4631
RotemAmir22 <100492679+RotemAmir22@users.noreply.github.com>
2023-05-07 12:08:49 +0300

    DataBase class

---
+++ dev/src/DataAccess/Database.java
@@ -0,0 +1,7 @@
+package DataAccess;
+
+import java.sql.*;
+public class Database {
+    private static final String DB_URL = "jdbc:sqlite:test.db";public static Connection connect() throws SQLException {return DriverManager.getConnection(DB_URL);
+    }
+}
